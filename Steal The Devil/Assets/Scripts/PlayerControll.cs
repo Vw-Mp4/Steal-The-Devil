7ca55524d9479179903c0e09f65c8b3265a5af05
Vw-Mp4 <victorwilsonbmcandido29@gmail.com>
2024-09-19 20:06:21 -0300

    CAMERA, E CENA 1
    Ajeitei levemente a câmera... mas gostaria de um resultado mais satisfatório. O cenário base tá montado, mas é tremendamente arcaico. Vou colocar mais coisinhas nele, e passar pros modeladores logo.
    Preciso ajeitar o personagem e penso já mexer no mixamo.

--- Steal The Devil/Assets/Scripts/PlayerControll.cs
+++ Steal The Devil/Assets/Scripts/PlayerControll.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,12 @@ public class PlayerControll : MonoBehaviour
 {
     public float horizontalInput;
     public float verticalInput;
+    public float mouseX;
+    public float mouseY;
     public float speed;
+    public float rotationValue;
+    
+
     Cinemachine.CinemachineImpulseSource source;
 
     // Start is called before the first frame update
@@ -20,13 +26,21 @@ public class PlayerControll : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
         Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
         transform.Translate(direction * Time.deltaTime * speed);
+        
 
         /*if (Input.GetKey(KeyCode.LeftShift))
         {
             source = GetComponent<Cinemachine.CinemachineImpulseSource>();
             source.GenerateImpulse(Camera.main.transform.forward);
         }*/ // <------------------ ISSO É PRA DEPOIS.  
+    }
+
+    private void LateUpdate()
+    {
+        transform.Rotate(Vector3.up * rotationValue * mouseX * Time.deltaTime);
     }
 }
