4e72b31381fb4278878d5aa007053227eec8e359
Vw-Mp4 <victorwilsonbmcandido29@gmail.com>
2024-10-13 15:25:48 -0300

    Interação
    interação com porta e bau funcionando; no processo de implementar o armario

--- Steal The Devil/Assets/Scripts/Interactor/Chest.cs
+++ Steal The Devil/Assets/Scripts/Interactor/Chest.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class Chest : MonoBehaviour, IInteractable
 {
     [SerializeField] private string _prompt;
+    public GameObject bau;
+    public Animator chestAnim;
 
     public string InteractionPrompt => _prompt;
 
+    void Start()
+    {
+        bau = GameObject.FindGameObjectWithTag("bau");
+        chestAnim = bau.GetComponent<Animator>();
+    }
     public bool Interact(Interactor interactor)
     {
         Debug.Log("eu sou gay");
+        chestAnim.SetBool("chestOpening", true);
         return true;
     }
 }
--- Steal The Devil/Assets/Scripts/Interactor/Inventory.cs
+++ Steal The Devil/Assets/Scripts/Interactor/Inventory.cs
@@ -1,19 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class Inventory : MonoBehaviour
 {
     public bool hasKey = false;
+    public bool chestOpen = false;
+    public float raycastDistance = 2.0f;
+    public float radiusDistance = 3.0f;
+    public float circusDistanece = 2.0f;
+    float forceMagnitude = 1f;
+    public GameObject chave;
+    public GameObject armario;
+    Animator animator;
 
+    private void Awake()
+    {
+        chave = GameObject.FindGameObjectWithTag("chave");
+        armario = GameObject.FindGameObjectWithTag("armario");
+        chestOpen = !chestOpen;
+    }
     private void Update()
     {
-
         //Se eu apertar Q, vou ter a chave.
-        if(Keyboard.current.qKey.wasPressedThisFrame)
+        if (Keyboard.current.eKey.wasPressedThisFrame)
         {
-            hasKey = !hasKey;
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, raycastDistance))
+            {
+                if (hit.collider.gameObject == chave)
+                {
+                    Destroy(chave);
+                    hasKey = !hasKey;
+                    Debug.DrawLine(transform.position, transform.forward);
+                }
+            }
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody rigidbody = hit.collider.attachedRigidbody;
+        if (rigidbody != null)
+        {
+            
+                Vector3 forceDirection = hit.gameObject.transform.position - transform.position;
+                forceDirection.y = 0;
+                forceDirection.z = 0;
+                forceDirection.Normalize();
+
+                rigidbody.AddForceAtPosition(forceDirection * forceMagnitude, transform.position, ForceMode.Impulse);
+            
+            
         }
     }
 }
